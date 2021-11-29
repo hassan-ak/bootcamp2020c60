d45e22512a8dd602ae780f9870a12397728100e8
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-11-29 16:17:41 +0500

    create cdk app

---
+++ step12_grant_iam_policy_to_resources/README.md
@@ -0,0 +1,8 @@
+# Step 12 Granting IAM policies to Resources
+
+## Steps to code
+
+1. Create a new directory by using `mkdir step12_grant_iam_policy_to_resources`
+2. Naviagte to the newly created directory using `cd step12_grant_iam_policy_to_resources`
+3. Create a cdk app using `cdk init app --language typescript`
+4. Use `npm run watch` to auto build our app as we code
---
+++ step12_grant_iam_policy_to_resources/test/step12_grant_iam_policy_to_resources.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as Step12GrantIamPolicyToResources from '../lib/step12_grant_iam_policy_to_resources-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step12_grant_iam_policy_to_resources-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step12GrantIamPolicyToResources.Step12GrantIamPolicyToResourcesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
